ae8b8523ea1e8b49009f2edb2416b85f77d4f27d
Omolayo Victor <remymartins0398@yahoo.com>
2021-03-27 02:33:02 +0100

    Add shared module

---
+++ src/app/shared/modules/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { LoaderComponent } from '../components/loader/loader.component';
+
+@NgModule({
+  declarations: [
+    LoaderComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    LoaderComponent
+  ]
+})
+export class SharedModule { }
