44c2bf84f4cd59eb82b6a6f61e2ac8fdadcfa158
Omolayo Victor <remymartins0398@yahoo.com>
2021-03-27 02:32:04 +0100

    Restructure imports and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
-import { Routes, RouterModule, PreloadAllModules, ExtraOptions } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { EventContainerComponent } from './components/event-container/event-container.component';
+import { ListEventComponent } from './components/list-event/list-event.component';
 
 const routes: Routes = [
   {
-    path:'',
-    children:[
-      { path: '', redirectTo: '/event', pathMatch: 'full' },
-      { path: 'event', component: EventContainerComponent },
-
-      { path: '**', redirectTo: '/event' },
-    ]
-  }
-
+    path: '',
+    children: [
+      { path: '', redirectTo: '/events', pathMatch: 'full' },
+      { path: 'events', component: ListEventComponent },
+      {
+        path: 'event/:id',
+        loadChildren: () => import('src/app/modules/checkout/checkout.module').then((module) => module.CheckoutModule),
+      },
+      { path: '**', redirectTo: '/events' },
+    ],
+  },
 ];
-
 
 @NgModule({
   imports: [
